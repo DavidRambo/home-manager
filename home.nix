087b60c0b208f702725cfafd91a88e8e0951d126
David Rambo <davidrambo@mailfence.com>
2024-06-28 21:51:55 -0800

    with pkgs;

--- home.nix
+++ home.nix
@@ -18,16 +18,16 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
 
-    pkgs.erdtree
-    pkgs.fd
-    pkgs.fzf
-    pkgs.lsd
-    pkgs.ripgrep
+    erdtree
+    fd
+    fzf
+    lsd
+    ripgrep
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
