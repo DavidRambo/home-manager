fba8d9b9e92e570f3a4db612b5ba8b3b8d2c5127
David Rambo <davidrambo@mailfence.com>
2025-11-20 08:26:14 -0800

    change env var for xdg config to explicit absolute path

--- home.nix
+++ home.nix
@@ -73,8 +73,8 @@
     XDG_CACHE_HOME = "/Users/david/.cache";
     XDG_CONFIG_HOME = "/Users/david/.config";
     XDG_STATE_HOME = "/Users/david/.local/state";
-    ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
     EDITOR = "~/.nix-profile/bin/nvim";
+    ZDOTDIR = "/Users/david/.config/zsh";
     LEDGER_FILE = "~/finance/2024.journal";
   };
 
--- zsh.nix
+++ zsh.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.zsh = {
     enable = true;
-    dotDir = "$XDG_CONFIG_HOME/zsh";
+    dotDir = "/Users/david/.config/zsh";
     # history.path = "$ZDOTDIR/.zsh_history";
     enableCompletion = true;
     autosuggestion.enable = true;
